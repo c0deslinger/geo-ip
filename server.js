498eda1c7e74ae2d3c12ba602689053bd78a8794
Mochammad Yusuf Fachroni <yusuf.fachroni@gmail.com>
2024-06-29 22:19:01 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import geoip from "geoip-lite";
+
+const app = express();
+const port = 3000;
+
+app.get("/geoip", (req, res) => {
+  let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+  // For local testing, use a known IP address
+  if (ip === "::1" || ip === "127.0.0.1") {
+    ip = "8.8.8.8"; // Google's public DNS server IP address for testing
+  }
+
+  const geo = geoip.lookup(ip);
+
+  if (geo) {
+    const { country, region, city } = geo;
+    return res.json({
+      ip,
+      country,
+      countryName: geoip.pretty(country),
+      region,
+      city,
+    });
+  } else {
+    return res.status(404).json({ message: "Location not found" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
